b8eb55e5af3f95c30255682fee0f741d55ded4a3
michaellange <michaellange@users.noreply.github.com>
2015-06-06 09:03:05 +0200

    Plot3

---
+++ plot3.R
@@ -0,0 +1,32 @@
+## Download zip if not exists
+file <- file.path(getwd(), "household_power_consumption.zip")
+if (!file.exists("household_power_consumption.zip")) {
+  url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(url, file)
+}
+
+## Read full data
+data_org <- read.csv(unz(file, "household_power_consumption.txt"), sep=';', na.strings="?", stringsAsFactors=F)
+
+## Find the correct subset
+data_org$Date <- as.Date(data_org$Date, format="%d/%m/%Y")
+data_subset <- subset(data_org, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+## Remove full data
+rm(data_org)
+
+## Create Datetime out of Date and Time
+datetime <- paste(data_subset$Date, data_subset$Time)
+data_subset$Datetime <- as.POSIXct(datetime)
+
+## Plot it and save it to a file
+png("plot3.png", width=480, height=480)
+
+with(data_subset, {
+  plot(Sub_metering_1~Datetime, type="l", ylab="Energy sub metering", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lwd=1, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+dev.off()
